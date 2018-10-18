8fc6d10b9825a94e98a1b065b8ad6978701301f2
Cody Rickman <cody@creativemindstudios.com>
2018-10-18 19:31:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def name = (dog_name)
+  def name=(dog_name)
     @this_dogs_name = dog_name
   end
   def name
     puts @this_dogs_name
   end
-  def breed = (dog_breed)
+  def breed=(dog_breed)
     @this_dogs_breed = dog_breed
   end
   def breed
