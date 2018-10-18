aeb9d66dddad2b57511e1dc655e237a4bceb2ca0
Cody Rickman <cody@creativemindstudios.com>
2018-10-18 19:44:09 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,12 +3,12 @@ class Person
     @name = persons_nmae
   end
   def name
-    puts name
+    puts @name
   end
   def job=(persons_job)
     @job = persons_job
   end
   def job
-    puts job
+    puts @job
   end
 end
